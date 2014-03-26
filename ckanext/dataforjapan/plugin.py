cca37243f3af257348cd6581cc908a424ace8180
Fumihiro Kato <fumi@fumi.me>
2014-03-26 17:57:26 +0900

    Added plugin.py

---
+++ ckanext/dataforjapan/plugin.py
@@ -0,0 +1,13 @@
+''' plugin.py
+'''
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+class DataForJapanThemePlugin(plugins.SingletonPlugin):
+  '''
+      CKAN theme plugin for Data for Japan
+  '''
+  plugins.implements(plugins.IConfigurer)
+
+  def update_config(self, config):
+    toolkit.add_template_directory(config, 'templates')
